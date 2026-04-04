551cb694ed5711de1a48b2ed4b274753864c58cc
AJITH V <113958920+AjithQA-hub@users.noreply.github.com>
2026-04-04 19:40:56 +0530

    Update JavaScriptTask3.java

--- JavaScript/src/org/javascript/JavaScriptTask3.java
+++ JavaScript/src/org/javascript/JavaScriptTask3.java
@@ -11,17 +11,13 @@ public class JavaScriptTask3 {
 	public static void main(String[] args) {
 		
         System.setProperty("webdriver.chrome.driver","C:\\Users\\AJITH\\eclipse-workspace\\JavaScript\\Drivers\\chromedriver.exe");
-		
 		WebDriver driver=new ChromeDriver();
-		
 		driver.get("https://inmakesedu.com/");
-		
 		driver.manage().window().maximize();
-		
+	
 		JavascriptExecutor js=(JavascriptExecutor)driver;
 		   
 		   WebElement a1= driver.findElement(By.xpath("//h2[contains(text(),'Programs')]"));
 		   js.executeScript("arguments[0].scrollIntoView(true)", a1);
 	}
-
 }
